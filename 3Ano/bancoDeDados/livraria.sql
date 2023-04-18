f8e4ab2c6e72ce9d35d7ab951e61b32693906eb2
cemeterydriiver <ashlynierow@gmail.com>
2023-04-18 20:22:32 -0300

    justificativas

--- 3Ano/bancoDeDados/livraria.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE livro(
-  genero VARCHAR(100) not NULL,
-  autor VARCHAR(100) not NULL,
-  ISBN_PK VARCHAR(13) not NULL,
-  editora VARCHAR(100) not NULL,
-  titulo VARCHAR(100) not NULL,
-  PRIMARY KEY (ISBN_PK)
-  )
---
+++ 3Ano/diagramaClasses/diagramasRpg/README.md
@@ -0,0 +1,2 @@
+## Por que escolhi essa atividade?
+> Escolhi essa atividade pois como gosto muito de RPG, foi legal criar um diagrama de classe sobre o assunto.
