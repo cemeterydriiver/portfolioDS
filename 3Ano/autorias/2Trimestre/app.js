9f1b7991e4f55bc282d4962a8dc9c12cbcd89289
cemeterydriiver <ana_clara-souza@estudante.sesisenai.org.br>
2023-08-07 10:48:21 -0300

    add

---
+++ 3Ano/autorias/2Trimestre/app.js
@@ -0,0 +1,216 @@
+const allTabsBody = document.querySelectorAll('.tab-body-single');
+const allTabsHead = document.querySelectorAll('.tab-head-single');
+const searchForm = document.querySelector('.app-header-search');
+let searchList = document.getElementById('search-list');
+
+let activeTab = 1, allData;
+
+const init = () => {
+    showActiveTabBody();
+    showActiveTabHead();
+}
+
+const showActiveTabHead = () => allTabsHead[activeTab - 1].classList.add('active-tab');
+
+const showActiveTabBody = () => {
+    hideAllTabBody();
+    allTabsBody[activeTab - 1].classList.add('show-tab');
+}
+
+const hideAllTabBody = () => allTabsBody.forEach(singleTabBody => singleTabBody.classList.remove('show-tab'));
+const hideAllTabHead = () => allTabsHead.forEach(singleTabHead => singleTabHead.classList.remove('active-tab'));
+
+window.addEventListener('DOMContentLoaded', () => init());
+
+allTabsHead.forEach(singleTabHead => {
+    singleTabHead.addEventListener('click', () => {
+        hideAllTabHead();
+        activeTab = singleTabHead.dataset.id;
+        showActiveTabHead();
+        showActiveTabBody();
+    });
+});
+
+const getInputValue = (event) => {
+    event.preventDefault();
+    let searchText = searchForm.search.value;
+    fetchAllSuperHero(searchText);
+}
+
+
+searchForm.addEventListener('submit', getInputValue);
+
+
+const fetchAllSuperHero = async(searchText) => {
+    let url = `https://www.superheroapi.com/api.php/727054372039115/search/${searchText}`;
+    try{
+        const response = await fetch(url);
+        allData = await response.json();
+        if(allData.response === 'success'){
+        
+            showSearchList(allData.results);
+        }
+    } catch(error){
+        console.log(error);
+    }
+}
+
+const showSearchList = (data) => {
+    searchList.innerHTML = "";
+    data.forEach(dataItem => {
+        const divElem = document.createElement('div');
+        divElem.classList.add('search-list-item');
+        divElem.innerHTML = `
+            <img src = "${dataItem.image.url ? dataItem.image.url : ""}" alt = "">
+            <p data-id = "${dataItem.id}">${dataItem.name}</p>
+        `;
+        searchList.appendChild(divElem);
+    });
+}
+
+searchForm.search.addEventListener('keyup', () => {
+    if(searchForm.search.value.length > 1){
+        fetchAllSuperHero(searchForm.search.value);
+    } else {
+        searchList.innerHTML = "";
+    }
+});
+
+searchList.addEventListener('click', (event) => {
+    let searchId = event.target.dataset.id;
+    let singleData = allData.results.filter(singleData => {
+        return searchId === singleData.id;
+    })
+    showSuperheroDetails(singleData);
+    searchList.innerHTML = "";
+});
+
+const showSuperheroDetails = (data) => {
+    console.log(data);
+    document.querySelector('.app-body-content-thumbnail').innerHTML = `
+        <img src = "${data[0].image.url}">
+    `;
+
+    document.querySelector('.name').textContent = data[0].name;
+    document.querySelector('.powerstats').innerHTML = `
+    <li>
+        <div>
+            <i class = "fa-solid fa-shield-halved"></i>
+            <span>Inteligência</span>
+        </div>
+        <span>${data[0].powerstats.intelligence}</span>
+    </li>
+    <li>
+        <div>
+            <i class = "fa-solid fa-shield-halved"></i>
+            <span>Força</span>
+        </div>
+        <span>${data[0].powerstats.strength}</span>
+    </li>
+    <li>
+        <div>
+            <i class = "fa-solid fa-shield-halved"></i>
+            <span>velocidade</span>
+        </div>
+        <span>${data[0].powerstats.speed}</span>
+    </li>
+    <li>
+        <div>
+            <i class = "fa-solid fa-shield-halved"></i>
+            <span>Durabilidade</span>
+        </div>
+        <span>${data[0].powerstats.durability}</span>
+    </li>
+    <li>
+        <div>
+            <i class = "fa-solid fa-shield-halved"></i>
+            <span>Poder</span>
+        </div>
+        <span>${data[0].powerstats.power}</span>
+    </li>
+    <li>
+        <div>
+            <i class = "fa-solid fa-shield-halved"></i>
+            <span>combate</span>
+        </div>
+        <span>${data[0].powerstats.combat}</span>
+    </li>
+    `;
+
+    document.querySelector('.biography').innerHTML = `
+    <li>
+        <span>Nome completo</span>
+        <span>${data[0].biography['full-name']}</span>
+    </li>
+    <li>
+        <span>Alter egos</span>
+        <span>${data[0].biography['alter-egos']}</span>
+    </li>
+    <li>
+        <span>Parentes</span>
+        <span>${data[0].biography['aliases']}</span>
+    </li>
+    <li>
+        <span>Local de aniversário</span>
+        <span>${data[0].biography['place-of-birth']}</span>
+    </li>
+    <li>
+        <span>Primeira aparição</span>
+        <span>${data[0].biography['first-appearance']}</span>
+    </li>
+    <li>
+        <span>Editora</span>
+        <span>${data[0].biography['publisher']}</span>
+    </li>
+    `;
+
+    document.querySelector('.appearance').innerHTML = `
+    <li>
+        <span>
+            <i class = "fas fa-star"></i> Gênero
+        </span>
+        <span>${data[0].appearance['gender']}</span>
+    </li>
+    <li>
+        <span>
+            <i class = "fas fa-star"></i> Raça
+        </span>
+        <span>${data[0].appearance['race']}</span>
+    </li>
+    <li>
+        <span>
+            <i class = "fas fa-star"></i> Altura
+        </span>
+        <span>${data[0].appearance['height'][0]}</span>
+    </li>
+    <li>
+        <span>
+            <i class = "fas fa-star"></i> Peso
+        </span>
+        <span>${data[0].appearance['weight'][0]}</span>
+    </li>
+    <li>
+        <span>
+            <i class = "fas fa-star"></i> Cor dos olhos
+        </span>
+        <span>${data[0].appearance['eye-color']}</span>
+    </li>
+    <li>
+        <span>
+            <i class = "fas fa-star"></i> Cor do cabelo
+        </span>
+        <span>${data[0].appearance['hair-color']}</span>
+    </li>
+    `;
+
+    document.querySelector('.connections').innerHTML = `
+    <li>
+        <span>Grupos afiliados</span>
+        <span>${data[0].connections['group-affiliation']}</span>
+    </li>
+    <li>
+        <span>Parentes</span>
+        <span>${data[0].connections['relatives']}</span>
+    </li>
+    `;
+}
---
+++ 3Ano/autorias/2Trimestre/readme.md
@@ -0,0 +1,5 @@
+# Autoria 2º trimestre
+> Como autoria do 2º trimestre, fiz um site simples que consume uma [API de heróis](https://superheroapi.com/).
+
+## Sobre a autoria:
+> O GoogleHero como o nome diz, é basicamente um Google de super-heróis. Aqui você pode pesquisar qualquer super-heróis e o site retornará o mesmo com os status do super-herói pesquisado, biografia, aparência e conexões.
